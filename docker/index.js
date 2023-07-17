bf253516f4131d045dd95a72ce59a6be378dacee
gabriele.carassale <gabriele.carassale@vidiemme.it>
2023-07-17 17:49:49 +0200

    :zap: wip Docker module

--- docker/index.js
+++ docker/index.js
@@ -2,8 +2,8 @@ const { template} = require('mrm-core')
 const { join } = require('path')
 
 function task() {
-    template('Dockerfile', join(__dirname, 'templates', 'Dockerfile')).apply().save()
-    template('scripts/start.sh', join(__dirname, 'templates', 'start.sh')).apply().save()
+    template('Dockerfile', join(__dirname, 'templates', 'Dockerfile.txt')).apply().save()
+    template('scripts/start.sh', join(__dirname, 'templates', 'scripts_start.sh.txt')).apply().save()
 }
 
 task.description = 'Add base Dockerfile and the start.sh to the project'
